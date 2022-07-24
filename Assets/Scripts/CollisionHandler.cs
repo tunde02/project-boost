4de5b1678f50cc44750c521cdc6099ebacb98611
SeongHo Jin <snskrktja@naver.com>
2022-07-24 09:00:52 +0900

    로켓 충돌시 Scene을 불러오는 부분 추가

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,44 @@
+using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+    void OnCollisionEnter(Collision other)
+    {
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("tag : Friendly");
+                break;
+            case "Finish":
+                LoadNextLevel();
+                break;
+            case "Fuel":
+                Debug.Log("tag : Fuel");
+                break;
+            default:
+                ReloadLevel();
+                break;
+        }
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentSceneIndex);
+    }
+}
